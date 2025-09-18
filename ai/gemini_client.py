256a5db2bffc7a036b82e39df7872695268cfb8c
Sahil Kumar <166407765+Saahil-Kumaar@users.noreply.github.com>
2025-09-18 15:10:26 +0530

    Update gemini_client.py

--- ai/gemini_client.py
+++ ai/gemini_client.py
@@ -6,13 +6,6 @@ import streamlit as st
 from google.oauth2 import service_account
 
 class GeminiProcessor:
-    # def __init__(self, project_id: str, location: str = "us-central1"):
-    #     vertexai.init(project=project_id, location=location)
-    #     self.model = GenerativeModel("gemini-2.5-flash")
-    # def __init__(self, project_id: str, location: str = "us-central1"):    
-    #     credentials = service_account.Credentials.from_service_account_info(st.secrets["firebase"])
-    #     vertexai.init(project=project_id, location=location, credentials=credentials)
-    #     self.model = GenerativeModel("gemini-2.5-flash")
     def __init__(self, project_id: str, location: str = "us-central1"):
         try:
             # Get credentials from Streamlit secrets
